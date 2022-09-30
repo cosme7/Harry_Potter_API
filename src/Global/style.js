e97bc30a42e88b9411af7b8bce56843e0bf41f2a
cosme7 <cosmewr@gmail.com>
2022-09-29 21:56:50 -0300

    Teste

--- src/Global/style.js
+++ src/Global/style.js
@@ -14,8 +14,8 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body{
-        width: 100%;
         min-height: 100vh;
+        width: 100%;
         background-image: url(${Background});
     }
 
